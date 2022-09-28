b095bcf0fda3c458cfdce6d14c74fbdbadefb57d
pscore23 <rubik2357x@gmail.com>
2022-09-28 21:20:16 +0900

    プロジェクト ファイルを追加します。

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Http;
+
+
+namespace HttpRequest {
+    public static class Program {
+        private static readonly Action<string> write = (text) => Console.WriteLine(text);
+        private static readonly HttpClient _HttpClient = new HttpClient();
+
+        public static void Main(string[] _) {
+            string url = "https://scratch.mit.edu/";
+
+            var HeadMessage = new HttpRequestMessage(HttpMethod.Head, url);
+            var Response = _HttpClient.SendAsync(HeadMessage).Result;
+            var Headers = Response.Headers;
+
+            write($"ETag: {Headers.ETag?.Tag}");
+        }
+    }
+}
